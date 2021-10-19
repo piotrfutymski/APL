f2b5482997b9726d6fedcda0ef5a20c0c1001644
Jakub Kasprzak <jjkasprzak@protonmail.com>
2021-10-19 16:50:23 +0200

    add Requests

--- src/main/js/App.js
+++ src/main/js/App.js
@@ -2,15 +2,6 @@ const axios = require('axios');
 import React, {useState, useEffect} from 'react'
 
 const App = () => {
-	useEffect(()=>{
-	    axios.get('/apl-api/experiment')
-                  .then(function (response) {
-                    setHello(response.data.x);
-                  })
-                  .catch(function (error) {
-                    console.log(error);
-                  });
-	},[]);
 	return (
 	<div>
 	    <h1>TEST</h1>
