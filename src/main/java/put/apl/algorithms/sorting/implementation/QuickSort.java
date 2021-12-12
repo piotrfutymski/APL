36de0c56ce5d42cdff3cfd169634dfd24ca33298
Jakub Kasprzak <73919276+jjkasprzak@users.noreply.github.com>
2021-12-12 16:30:21 +0100

    41 quick sort fix (#57)
    * quick sort median strategy fix
    
    * fix shuffle
    
    * change to randomize median selection

--- src/main/java/put/apl/algorithms/sorting/implementation/QuickSort.java
+++ src/main/java/put/apl/algorithms/sorting/implementation/QuickSort.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Component;
 import put.apl.algorithms.sorting.data.SortingData;
 import put.apl.algorithms.sorting.SortingResult;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Map;
 
 @Component("QuickSort")
@@ -24,12 +26,45 @@ public class QuickSort implements SortingAlgorithm{
                 int l = left;
                 int m = (left+right)/2;
                 int r = right-1;
-                if (!tab.lessEqual(l, m) ^ !tab.lessEqual(l, r))
+                if ((tab.lessEqual(m, l) && tab.lessEqual(l, r)) || (tab.lessEqual(r, l) && tab.lessEqual(l, m)))
                     return l;
-                else if (tab.less(m, l) ^ !tab.less(m, r))
+                else if ((tab.lessEqual(l, m) && tab.lessEqual(m, r)) || (tab.lessEqual(r, m) && tab.lessEqual(m, l)))
                     return m;
-                else
-                    return r;
+                return r;
+            case "Median" :
+                if (medianCount == null) {
+                    throw new IllegalStateException("Undefined medianCount parameter for QuickSort");
+                }
+                SortingData medianData = new SortingData(Arrays.copyOfRange(tab.getTab(), left, right+1));
+                medianData.shuffle();
+
+                int tmpl = 0;
+                int tmpr = medianCount-1;
+                if(right-left < tmpr)
+                    tmpr = right-left;
+                int desiredp = (tmpl+tmpr)/2;
+
+                int p = -1;
+                while(p != desiredp) {
+                    tab.escape();
+                    p = partition(medianData, tmpl, tmpr, (tmpl+tmpr)/2);
+                    if(p < desiredp){
+                        tmpl = p+1;
+                    }
+                    else if (p > desiredp){
+                        tmpr = p-1;
+                    }
+                }
+                int val = medianData.getTab()[p];
+                tab.setCompCount(tab.getCompCount() + medianData.getCompCount());
+                tab.setSwapCount(tab.getSwapCount() + medianData.getSwapCount());
+                for(int i = left; i <= right; i++){
+                    if(tab.getTab()[i] == val){
+                        return i;
+                    }
+                }
+                throw new IllegalStateException("Median pivot exception");
+
             default:
                 throw new IllegalStateException("Illegal pivot Strategy for QuickSort");
 
@@ -94,22 +129,11 @@ public class QuickSort implements SortingAlgorithm{
             quickSort(tab, m+1, r);
         }
     }
-    private void quickSortWithMedian(SortingData tab, int l, int r) throws InterruptedException {
-        if(l<r){
-            int m = partitionWithMedian(tab, l, r);
-            quickSortWithMedian(tab, l, m-1);
-            quickSortWithMedian(tab, m+1, r);
-        }
-    }
 
     @Override
     public SortingResult sort(SortingData tab) throws InterruptedException {
-        if(pivotStrategy.equals("Median")){
-            quickSortWithMedian(tab, 0, tab.length()-1);
-        }
-        else {
-            quickSort(tab, 0, tab.length()-1);
-        }
+        quickSort(tab, 0, tab.length()-1);
+
         return SortingResult.builder()
                 .comparisonCount(tab.getCompCount())
                 .swapCount(tab.getSwapCount())
