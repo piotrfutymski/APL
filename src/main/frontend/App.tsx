325923a2464a23ce7d911853c0084d4da9932fb3
Jakub Kasprzak <jjkasprzak@protonmail.com>
2021-12-20 23:00:59 +0100

    add cookie prompt

--- src/main/frontend/App.tsx
+++ src/main/frontend/App.tsx
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import { CookiesProvider } from 'react-cookie';
+import { CookiesProvider, useCookies } from 'react-cookie';
 
 import { Header, Subpage } from './header/Header';
 import { Home } from './home/Home';
@@ -14,16 +14,44 @@ export const App = () => {
 		{link: "/experiments/graph", label:"GRAPH ALGORITHMS"}
 	]
 
+	const [cookies, setCookie] = useCookies(['AllowCookies']);
+	const allowCookies = (cookies.AllowCookies || 'false')
 	return (
 		<CookiesProvider>
 			<BrowserRouter>
-				<div className={styles.Header}>
-					<Header subpages={subpages} showLogo={true} />
-				</div>
-				<Routes>
-					<Route path="/" element={<Home />}/>
-					<Route path="/experiments/*" element={<ExperimentView />} />
-				</Routes>
+				{
+					allowCookies === 'true' ? 
+					<>
+						<div className={styles.Header}>
+							<Header subpages={subpages} showLogo={true} />
+						</div>
+						<Routes>
+							<Route path="/" element={<Home />}/>
+							<Route path="/experiments/*" element={<ExperimentView />} />
+						</Routes>
+					</> :
+					<div className={styles.CookieForm}>
+						<div className={styles.Content}>
+							<div>
+								<h1>Cookies</h1>
+								<p>
+									This website use cookies to improve user experience.<br /> 
+									<b>Information collected:</b>
+									<ul>
+										<li>Recent experiment configurations</li>
+									</ul>
+									<b>Information NOT collected:</b>
+									<ul>
+										<li>Personal information</li>
+									</ul>
+								</p>
+							</div>
+							<button onClick={()=>{setCookie('AllowCookies', 'true', {path: '/', sameSite: true, maxAge: 36288000})}}>
+								Allow Cookies
+							</button>
+						</div>
+					</div>
+				}
 			</BrowserRouter>
 		</CookiesProvider>
 	)
