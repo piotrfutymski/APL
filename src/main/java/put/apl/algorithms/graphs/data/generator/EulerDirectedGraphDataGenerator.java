f2ab6816a7bb6232ad3a7c00e076bc4cba086094
Piotr Futymski <piotr.futymski@wp.pl>
2022-01-11 20:58:27 +0100

    euler fixes

--- src/main/java/put/apl/algorithms/graphs/data/generator/EulerDirectedGraphDataGenerator.java
+++ src/main/java/put/apl/algorithms/graphs/data/generator/EulerDirectedGraphDataGenerator.java
@@ -17,21 +17,16 @@ public class EulerDirectedGraphDataGenerator extends GraphDataGenerator {
         int added = 0;
 
         List<Integer> toUse = IntStream.range(0, config.getNumberOfVertices()).boxed().collect(Collectors.toList());
-        if(toUse.size() % 3 == 1)
-            toUse.add(0);
-        if(toUse.size() % 3 == 2)
-            toUse.add(1);
         Collections.shuffle(toUse);
 
-        while (added < toAdd || toUse.size() > 0){
+        while (added < toAdd || !toUse.isEmpty()){
             escape();
-            int i = getNext(toUse, res, random);
-            int j = getNext(toUse, res, random);
-            int k = getNext(toUse, res, random);
-            if(i == j || j == k || i == k)
-                continue;
-            if(res.get(i).contains(j) || res.get(j).contains(k) || res.get(k).contains(i))
-                continue;
+            List<Integer> ijk = getNextEuler(toUse, res, random, true);
+            if(ijk.isEmpty())
+                break;
+            int i = ijk.get(0);
+            int j = ijk.get(1);
+            int k = ijk.get(2);
             res.get(i).add(j);
             res.get(j).add(k);
             res.get(k).add(i);
